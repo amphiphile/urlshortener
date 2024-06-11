2002ce48f494a90bd26e6511180ea7db345d7a30
Anna Shainurova <amphiphile@yandex.ru>
2024-06-12 02:03:40 +0300

    [increment-1] server initial implementation

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"github.com/amphiphile/urlshrtnr/internal/app"
+	"net/http"
+)
+
+func main() {
+	urlHandler := &app.UrlHandler{
+		Storage: &app.UrlStorage{
+			DBFileName: "db.json",
+		},
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, urlHandler.RequestHandler)
+	err := http.ListenAndServe(`:8080`, mux)
+
+	if err != nil {
+		panic(err)
+	}
+
+}
